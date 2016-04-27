625b508f30d5f31a3da57f0c6a1b85c559e32564
ppik <piksarv@gmail.com>
2016-04-27 11:10:26 +0100

    Created a simple random name generator

---
+++ rname_gen.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# Random name generator from letters of an input string
+
+import sys
+import random
+
+def gen_name(s, min_length=3):
+    length = random.randint(min_length, len(s))
+    return ''.join(random.sample(s, length))
+
+if __name__=="__main__":
+    s = ' '.join(argv[1:])
+    while True:
+        print(gen_name(s))
+        if input() > '':
+            break
+        
+    
