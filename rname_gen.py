56f76ad5fea7d06298b7f4052bdc046d5896d508
ppik <piksarv@gmail.com>
2016-04-27 11:16:10 +0100

    Fixed typo

--- rname_gen.py
+++ rname_gen.py
@@ -9,7 +9,7 @@ def gen_name(s, min_length=3):
     return ''.join(random.sample(s, length))
 
 if __name__ == "__main__":
-    s = ' '.join(argv[1:])
+    s = ' '.join(sys.argv[1:])
     while True:
         print(gen_name(s))
         if input() > '':
