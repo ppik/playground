b46944b6f1e0040b3da7798635b41c87ed13a3e0
ppik <piksarv@gmail.com>
2016-05-19 15:51:07 +0100

    Minor error corrections

--- test_array_vs_list.py
+++ test_array_vs_list.py
@@ -1,37 +1,47 @@
 #!/usr/bin/env python
-# From the preview of Dimitry Zinoviev's book "Data Science Essentials in
-# Python" <https://pragprog.com/book/dzpyds/data-science-essentials-in-python>
-# chapter 5 "Working with Tabular Numeric Data" unit 21 "Creating Arrays"
-# there was a note that states:
-#
-#   Contrary to their name, Python “lists” are actually implemented
-#   as arrays, not as lists. No storage is reserved for forward pointers,
-#   because no forward pointers are used. Large Python lists require
-#   only about 13% more storage than “real” numpy arrays. However,
-#   Python executes some simple built-in operations, such as sum(), 5–10
-#   times faster on lists than on arrays. Ask yourself if you really need
-#   any numpy-specific features before you start a numpy project!
-#
-# I have usually encountered the otherwise, but this script should test it.
-# --
-# Peeter Piksarv, 2015-05-13
+"""From the preview of Dimitry Zinoviev's book "Data Science Essentials in
+Python" <https://pragprog.com/book/dzpyds/data-science-essentials-in-python>
+chapter 5 "Working with Tabular Numeric Data" unit 21 "Creating Arrays"
+there was a note that states:
+
+    Contrary to their name, Python “lists” are actually implemented
+    as arrays, not as lists. No storage is reserved for forward pointers,
+    because no forward pointers are used. Large Python lists require
+    only about 13% more storage than “real” numpy arrays. However,
+    Python executes some simple built-in operations, such as sum(), 5–10
+    times faster on lists than on arrays. Ask yourself if you really need
+    any numpy-specific features before you start a numpy project!
+
+I have usually encountered the otherwise, but this script should test it.
+
+Update:
+Applying built-in function on a numpy-array is indeed ~2x slower than
+applying it on a list. Additionally, there is a speed penalty when using
+numpy's function on a list. However, numpy's functions on numpy's arrays are
+and array methods are much faster.
+--
+Peeter Piksarv, 2015-05-13
+"""
+
+import timeit
 
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import timeit
 
 # List of common functions between core python and numpy which both act same
 # over lists and arrays
-funcs = ['all', 'any', 'max', 'min', 'sum']
+FUNCS = ['all', 'any', 'max', 'min', 'sum']
 
 
-def time_array_vs_list(func='sum', N=10000):
+def time_array_vs_list(func='sum', num=10000):
+    """Times the particular function on a random list of a size of num.
+    """
 
-    if func not in funcs:
+    if func not in FUNCS:
         func = 'sum'
 
-    num_array = np.random.rand(N)
+    num_array = np.random.rand(num)
     num_list = list(num_array)
 
     # Arguments for timeit.timeit
@@ -55,22 +65,31 @@ def time_array_vs_list(func='sum', N=10000):
     return timings
 
 
-if __name__=='__main__':
-    
-    N = [2**n for n in range(1,12,2)]
+if __name__ == '__main__':
+
+    N = [2**n for n in range(1, 12, 2)]
     columns = ['N', 'func(list)', 'func(array)',
                'np.func(list)', 'np.func(array)', 'array.func()']
-    
-    for func in funcs:
+
+    plot_results = False
+    print_results = True
+
+    for func in FUNCS:
 
         # Timing calculations for different size list/arrays for func
-        data = np.array([[n, *time_array_vs_list(func='sum', N=n)] for n in N])
+        data = np.array([[n, *time_array_vs_list(func='sum', num=n)] for n in N])
 
         df = pd.DataFrame(data, index=N, columns=columns)
 
-        # Plot results
-        fig = plt.figure()
-        ax = fig.add_subplot(1,1,1)
-        df.plot(x='N', ax=ax, title='func = {}()'.format(func))
-        ax.set_ylabel('t (ms)')
-        fig.savefig('test_array_vs_list-{}-run{}.png'.format(func, str(pd.datetime.now())))
+        if plot_results:
+            fig = plt.figure()
+            ax = fig.add_subplot(1, 1, 1)
+            df.plot(x='N', ax=ax, title='func = {}()'.format(func))
+            ax.set_ylabel('t (ms)')
+            fig.savefig(
+                'test_array_vs_list-{}-run{}.png'.format(func, str(pd.datetime.now())))
+
+        if print_results:
+            print('func = {}()'.format(func))
+            print(df)
+            print()
